0a647e41707a0c052a10199fa2128e3c71a4f0f4
김지은 <100830777+Oeuni@users.noreply.github.com>
2025-07-22 09:31:18 +0900

    Update 대칭 차집합.java

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -12,22 +12,18 @@ public class Main {
 		int b = sc.nextInt();
 		
 		Set<Integer> setA = new HashSet<>();
-		Set<Integer> setACp = new HashSet<>();
 		for (int i = 0; i < a; i++) {
 			int x = sc.nextInt();
 			setA.add(x);
-			setACp.add(x);
 		}
 		
+		Set<Integer> setACp = new HashSet<>(setA);
 		Set<Integer> setB = new HashSet<>();
 		for (int i = 0; i < b; i++) {
 			setB.add(sc.nextInt());
 		}
 		
-		Set<Integer> chaA_B = new HashSet<>();
 		setA.removeAll(setB);
-		
-		Set<Integer> chaB_A = new HashSet<>();
 		setB.removeAll(setACp);
 		
 		System.out.println(setA.size() + setB.size());
