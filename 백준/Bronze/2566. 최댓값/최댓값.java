a28d4d92636b4954b01f604fa94dc94597d4ba1b
JiEun <100830777+Oeuni@users.noreply.github.com>
2025-07-28 13:09:59 +0900

    [Bronze III] Title: 최댓값, Time: 204 ms, Memory: 18452 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+// 백준 - 2566:최대값
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		int[][] arr = new int[9][9];
+		int max = 0, a = 0, b = 0;
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				arr[i][j] = sc.nextInt();
+				if (arr[i][j] > max) {
+					max = arr[i][j];
+					a = i;
+					b = j;
+				}
+			}
+		}
+		
+		System.out.println(max);
+		System.out.println((a+1) + " " + (b+1));
+	}
+}
