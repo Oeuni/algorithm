911ef9345a194ac1e13ee906f64de7b5a5370871
JiEun <100830777+Oeuni@users.noreply.github.com>
2025-09-23 17:03:14 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 22.39 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+			if (!map.containsKey(s.charAt(i)+"")) {
+				answer[i] = -1;
+				map.put(s.charAt(i)+"", i);
+			}
+			else {
+				answer[i] = i - map.get(s.charAt(i)+"");
+				map.put(s.charAt(i)+"", i);
+			}
+		}
+        return answer;
+    }
+}
