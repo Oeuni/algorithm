3709a1133c2e180fc5b75e638e0a3e21d9e81e67
JiEun <100830777+Oeuni@users.noreply.github.com>
2025-09-30 09:48:51 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.10 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,8 @@
+class Solution {
+    public long solution(long n) {
+        double answer = 0;
+        answer = Math.sqrt(n);
+
+        return answer % 1 == 0 ? (long)Math.pow(answer+1, 2) : -1;
+    }
+}
