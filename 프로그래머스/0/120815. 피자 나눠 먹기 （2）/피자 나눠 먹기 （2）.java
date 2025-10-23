8a8a7f2e120e52e00210d5d478a1adf1ae8092d1
JiEun <100830777+Oeuni@users.noreply.github.com>
2025-10-23 09:09:37 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        int pizza = 6;
+        while(pizza % n != 0) {
+        	pizza += 6;
+        	answer ++;
+        }
+        return answer;
+    }
+}
