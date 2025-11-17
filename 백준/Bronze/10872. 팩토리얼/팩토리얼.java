9fe552fbaff5726504aaa8723a2eec9359bcb194
JiEun <100830777+Oeuni@users.noreply.github.com>
2025-11-17 09:51:06 +0900

    [Bronze III] Title: 팩토리얼, Time: 168 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		System.out.println(fac(N));
+	}
+	
+	static int fac (int n) {
+		if (n <= 1) return 1;
+		return n * fac(n-1);
+	}
+}
